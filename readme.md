22153b316c76bd79e4d56ba98b080944874d8759
Your Name <you@example.com>
2018-10-24 17:47:44 +0300

    Verson_1

---
+++ readme.md
@@ -0,0 +1,25 @@
+## My Site
+
+Discription
+Discription
+Discription
+Discription
+Discription
+Discription
+Discription
+
+## Installation Description
+
+
+
+
+
+## Used technologies
+
+-apache php 7.2
+-PHP 7.2
+-MySQL 5.7
+-Laravel 5.7
+-Bootstrap
+-jQvery
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="utf-8"/>
+<meta name="description" content=""/>
+<meta name="keywords" content=""/>
+<meta name="author" content="KozlovskiyDN"/>
+<title>Site Name</title>
+<link type="text/css" rel="stylesheet" href="media/css/bootstrap.min.css"/>
+<link type="text/css" rel="stylesheet" href="media/css/style.css"/>
+
+
+
+</head>
+<body>
+<div id="header">
+<h1 id="logotxt">Site Name</h1>
+<img id="logo" src="media/img/logo.png"/>
+
+</div>
+<nav class="topmenu">
+<a href="#">Главная</a>
+<a href="#">О компании</a>
+<a href="#">Услуги</a>
+<a href="#">Товары</a>
+<a href="#">Контакты</a>
+</nav>
+<div >
+<div >
+<div class="col-md-2 moy1">
+<a href="#" class="btn btn-primary btn-block">Punkt menu 1</a>
+<a href="#" class="btn btn-primary btn-block">Punkt menu 2</a>
+<a href="#" class="btn btn-primary btn-block">Punkt menu 3</a>
+<a href="#" class="btn btn-primary btn-block">Punkt menu 4</a>
+<a href="#" class="btn btn-primary btn-block">Punkt menu 5</a>
+<a href="#" class="btn btn-primary btn-block">Punkt menu 6</a>
+<a href="#" class="btn btn-primary btn-block">Punkt menu 7</a>
+</div>
+<div class="col-md-8 moy">
+<h2>Dobruy Denb</h2>
+<p>
+Одно из наиболее популярных применений CSS — это скрытие подчеркивания у ссылок. С позиции юзабилити не совсем верное решение, поскольку пользователь может сразу не догадаться, что текст, который он видит, является ссылкой. Все ведь уже привыкли — раз подчеркивание используется, значит это ссылка. Но при правильном применении отсутствие подчеркивания у ссылок может придать определенный эффект сайту. Часто делается, что при наведении курсора, ссылка становится подчеркнутой, меняет свой цвет или используется и тот и другой эффект одновременно (пример 1). 
+</p>
+<p>
+Еще один пример демонстрирует использование в ссылках подчеркивания и надчеркивания одновременно. При этом эффекте тонкие линии будут появляться над и под ссылкой при наведении на нее курсора. Это достигается применением свойства text-decoration: underline overline в селекторе 
+</p>
+<p>
+Одно из наиболее популярных применений CSS — это скрытие подчеркивания у ссылок. С позиции юзабилити не совсем верное решение, поскольку пользователь может сразу не догадаться, что текст, который он видит, является ссылкой. Все ведь уже привыкли — раз подчеркивание используется, значит это ссылка. Но при правильном применении отсутствие подчеркивания у ссылок может придать определенный эффект сайту. Часто делается, что при наведении курсора, ссылка становится подчеркнутой, меняет свой цвет или используется и тот и другой эффект одновременно (пример 1). 
+</p>
+<p>
+Еще один пример демонстрирует использование в ссылках подчеркивания и надчеркивания одновременно. При этом эффекте тонкие линии будут появляться над и под ссылкой при наведении на нее курсора. Это достигается применением свойства text-decoration: underline overline в селекторе 
+</p>
+<p>
+Одно из наиболее популярных применений CSS — это скрытие подчеркивания у ссылок. С позиции юзабилити не совсем верное решение, поскольку пользователь может сразу не догадаться, что текст, который он видит, является ссылкой. Все ведь уже привыкли — раз подчеркивание используется, значит это ссылка. Но при правильном применении отсутствие подчеркивания у ссылок может придать определенный эффект сайту. Часто делается, что при наведении курсора, ссылка становится подчеркнутой, меняет свой цвет или используется и тот и другой эффект одновременно (пример 1). 
+</p>
+<p>
+Еще один пример демонстрирует использование в ссылках подчеркивания и надчеркивания одновременно. При этом эффекте тонкие линии будут появляться над и под ссылкой при наведении на нее курсора. Это достигается применением свойства text-decoration: underline overline в селекторе 
+</p>
+<p>
+Одно из наиболее популярных применений CSS — это скрытие подчеркивания у ссылок. С позиции юзабилити не совсем верное решение, поскольку пользователь может сразу не догадаться, что текст, который он видит, является ссылкой. Все ведь уже привыкли — раз подчеркивание используется, значит это ссылка. Но при правильном применении отсутствие подчеркивания у ссылок может придать определенный эффект сайту. Часто делается, что при наведении курсора, ссылка становится подчеркнутой, меняет свой цвет или используется и тот и другой эффект одновременно (пример 1). 
+</p>
+<p>
+Еще один пример демонстрирует использование в ссылках подчеркивания и надчеркивания одновременно. При этом эффекте тонкие линии будут появляться над и под ссылкой при наведении на нее курсора. Это достигается применением свойства text-decoration: underline overline в селекторе 
+</p>
+</div>
+<div class="col-md-2 moy">Vidget
+
+<table width="100%" height="180" style="  font-family:Tahoma; font-size:12px; color:#000000;" cellpadding="2" cellspacing="0">
+<tr><td><a href="http://6.pogoda.by/26850" style="font-family:Tahoma; font-size:12px; color:#003399;" title="Погода Минск на 6 дней - Гидрометцентр РБ" target="_blank">Погода Минск</a>
+</td></tr>
+<tr><td>
+
+<table width=100% height=100% style=" font-family:Tahoma; font-size:12px; color:#000000;" cellpadding="0" cellspacing="0">
+<tr><td>
+<script type="text/javascript" charset="windows-1251" src="http://pogoda.by/meteoinformer/js/26850_1.js"></script>
+</td></tr>
+</table>
+
+</td></tr>
+
+<tr><td align="right">Информация сайта <a href="http://www.pogoda.by" target="_blank" style="font-family:Tahoma; font-size:12px; color:#003399;">pogoda.by</a>
+</td></tr>
+</table>
+
+
+
+</div>
+</div>
+
+
+</div>
+
+<br style="clear:both"/>
+<div class="footer">
+2018 &copy; KozlovskiyDN
+</div>
+</body>
+</html>
+
+<!-- 
+<div> Zamenit</div>
+<article>Statby</article>
+<aside>fragment</aside>
+<figure>
+<img src="">
+<figcaption>opisanie</figcaption>
+</figure>
+<footer></footer>
+<header></header>
+<hgroup></hgroup>
+<nav></nav>
+<section></section>
+<span>strochnue</span>
+<time></time>
+<output></output>
+<mark></mark>
+<address></address>
+<div role="baner"></div>
+
+microdata - schema.org - standart!!!
+
+registraciya
+ -->
+
+
+
+
+
+
+
+
